d54fb64fbba5e02b6222a8960c4683edaf0f5131
aleksskorikov <aleksandrskorikov123@gmail.com>
2026-03-01 12:41:38 +0200

    Initial commit

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,19 +1,41 @@
-new Swiper('.swiper1', {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
-  pagination: { el: '.swiper-pagination1', clickable: true },
-  breakpoints: {
-    0: {
-      slidesPerView: 1
+// const swiper1 = new Swiper('.swiper1', {
+//     slidesPerView: 3,        // 3 слайда
+//     slidesPerGroup: 3,       // листает по 3
+//     spaceBetween: 24,
+//     loop: true,              // бесконечный
+
+//     pagination: {
+//         el: '.swiper-pagination1',
+//         clickable: true,
+//     },
+
+//     navigation: {
+//         nextEl: '.swiper-button-next1',
+//         prevEl: '.swiper-button-prev1',
+//     },
+
+//     speed: 600,
+// });
+
+const swiper1 = new Swiper('.swiper1', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    loop: true,
+    spaceBetween: 24,
+    speed: 600,
+
+    navigation: {
+        nextEl: '.slider-arrow--next',
+        prevEl: '.slider-arrow--prev',
     },
-    641: {
-      slidesPerView: 2,
-      spaceBetween: 20
+
+    pagination: {
+        el: '.slider-pagination',
+        clickable: true,
+        bulletClass: 'slider-dot',
+        bulletActiveClass: 'is-active',
+        renderBullet: function (index, className) {
+            return `<span class="${className}"></span>`;
+        },
     },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    }
-  }
 });
