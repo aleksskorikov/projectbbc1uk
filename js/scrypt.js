bf4eedb708d24819dbcd7166b05d0250d149e077
aleksskorikov <aleksandrskorikov123@gmail.com>
2026-03-07 18:35:20 +0200

    rty

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,36 @@
+// const swiper1 = new Swiper('.swiper1', {
+//     loop: true,
+//     speed: 600,
+//     spaceBetween: 24,
+
+//     navigation: {
+//         nextEl: '.slider-arrow--next',
+//         prevEl: '.slider-arrow--prev',
+//     },
+
+//     pagination: {
+//         el: '.slider-pagination',
+//         clickable: true,
+//         bulletClass: 'slider-dot',
+//         bulletActiveClass: 'is-active',
+//         renderBullet: function (index, className) {
+//             if (window.innerWidth <= 640 && index > 2) return '';
+//             return `<span class="${className}"></span>`;
+//         },
+//     },
+
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1,
+//             slidesPerGroup: 1,
+//         },
+//         641: {
+//             slidesPerView: 3,
+//             slidesPerGroup: 3,
+//         },
+//     },
+// });
+
 const swiper1 = new Swiper('.swiper1', {
     loop: true,
     speed: 600,
@@ -14,8 +47,12 @@ const swiper1 = new Swiper('.swiper1', {
         bulletClass: 'slider-dot',
         bulletActiveClass: 'is-active',
         renderBullet: function (index, className) {
-            if (window.innerWidth <= 640 && index > 2) return ''; 
-            return `<span class="${className}"></span>`;
+
+            if (window.innerWidth <= 640) {
+                if (index > 2) return '';
+            }
+
+            return `<span class="${className}" data-index="${index}"></span>`;
         },
     },
 
@@ -29,4 +66,21 @@ const swiper1 = new Swiper('.swiper1', {
             slidesPerGroup: 3,
         },
     },
+
+    on: {
+        slideChange: function () {
+
+            if (window.innerWidth <= 640) {
+                const bullets = document.querySelectorAll('.slider-dot');
+
+                bullets.forEach(b => b.classList.remove('is-active'));
+
+                const activeIndex = this.realIndex % 3;
+
+                if (bullets[activeIndex]) {
+                    bullets[activeIndex].classList.add('is-active');
+                }
+            }
+        }
+    }
 });
