ef88a4cc630374e07afb88ddd3423bc81f60db76
aleksskorikov <aleksandrskorikov123@gmail.com>
2026-03-07 16:49:48 +0200

    gfd

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,36 +1,3 @@
-// const swiper1 = new Swiper('.swiper1', {
-//     loop: true,
-//     speed: 600,
-//     spaceBetween: 24,
-
-//     navigation: {
-//         nextEl: '.slider-arrow--next',
-//         prevEl: '.slider-arrow--prev',
-//     },
-
-//     pagination: {
-//         el: '.slider-pagination',
-//         clickable: true,
-//         bulletClass: 'slider-dot',
-//         bulletActiveClass: 'is-active',
-//         renderBullet: function (index, className) {
-//             return `<span class="${className}"></span>`;
-//         },
-//     },
-
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1,
-//             slidesPerGroup: 1,
-//         },
-//         641: {
-//             slidesPerView: 3,
-//             slidesPerGroup: 3,
-//         },
-//     },
-// });
-
-
 const swiper1 = new Swiper('.swiper1', {
     loop: true,
     speed: 600,
